bba546bcbb18db25f5c5d8277fb2375b01802f1e
Furkan Kümüş <105515121+Furkan-Kumus@users.noreply.github.com>
2024-10-23 00:52:17 +0300

    project name change

--- ApartEv_UI_Angular/src/app/app.component.ts
+++ ApartEv_UI_Angular/src/app/app.component.ts
@@ -6,8 +6,8 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Apart_Ev_UI';
+  title = 'ApartEv_UI_Angular';
 }
