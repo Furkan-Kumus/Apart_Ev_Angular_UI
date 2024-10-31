09612b6dcc837168c6facff0374cbf1b1c077cac
Furkan Kümüş <105515121+Furkan-Kumus@users.noreply.github.com>
2024-10-31 11:26:32 +0300

    --no-standalone update

--- ApartEv_UI_Angular/src/app/app.component.ts
+++ ApartEv_UI_Angular/src/app/app.component.ts
@@ -1,10 +1,30 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+
+    RouterOutlet,
+    NzSpinModule,
+    NzFormModule,
+    NzButtonModule,
+    NzInputModule,
+    NzLayoutModule,
+    NzPageHeaderModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
