ba7c5c39ea23605507b3d365b34201ee2d653dcd
Furkan Kümüş <105515121+Furkan-Kumus@users.noreply.github.com>
2024-11-01 09:56:09 +0300

    auth service updated

--- apart_ev_ui/src/app/auth/services/storage/storage.service.ts
+++ apart_ev_ui/src/app/auth/services/storage/storage.service.ts
@@ -1,9 +1,54 @@
 import { Injectable } from '@angular/core';
 
+const TOKEN = "token";
+const USER = "user";
+
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class StorageService {
 
-  constructor() { }
+    constructor() { }
+
+    static saveToken(token: string): void {
+        window.localStorage.removeItem(TOKEN);
+        window.localStorage.setItem(TOKEN, token);
+    }
+
+    static saveUser(user: any): void {
+        window.localStorage.removeItem(USER);
+        window.localStorage.setItem(USER, JSON.stringify(user));
+    }
+
+    static getToken() {
+        return window.localStorage.getItem(TOKEN);
+    }
+    
+    static getUser() {//TODO: ???? orijinalinde böyle değil
+        const user = localStorage.getItem(USER);
+        if (user) {
+            return JSON.parse(user);
+        }
+        return null;
+    }
+    
+    static getUserRole(): string {
+        const user = this.getUser();
+        if (user == null) return "";
+        return user.role;
+    }
+    
+    static isAdminLoggedIn(): boolean {
+        if (this.getToken() == null) return false;
+        const role: string = this.getUserRole();
+        return role == "ADMIN";
+    }
+    
+    static isCustomerLoggedIn(): boolean {
+        if (this.getToken() == null) return false;
+        const role: string = this.getUserRole();
+        return role == "CUSTOMER";
+    }
+    
 }
+
