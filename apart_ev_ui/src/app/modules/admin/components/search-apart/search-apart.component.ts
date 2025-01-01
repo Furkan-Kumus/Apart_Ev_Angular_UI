6bce1a0eedea7c5dc8942d7c6dee5b5484bf6ee4
Furkan Kümüş <105515121+Furkan-Kumus@users.noreply.github.com>
2025-01-01 18:01:44 +0300

    language updated

--- apart_ev_ui/src/app/modules/admin/components/search-apart/search-apart.component.ts
+++ apart_ev_ui/src/app/modules/admin/components/search-apart/search-apart.component.ts
@@ -13,10 +13,10 @@ export class SearchApartComponent {
   aparts: any = [];
 
   listOfOption: Array<{ label: string; value: string }> = [];
-  listOfBrands = ["BMW", "AUDI", "FERRARI", "TESLA", "VOLVO", "TOYOTA", "HONDA", "FORD", "NISSAN", "HYUNDAI", "LEXUS", "KIA"];
-  listOfType = ["Petrol", "Hybrid", "Diesel", "Electric", "CNG"];
-  listOfColor = ["Red", "White", "Blue", "Black", "Orange", "Grey", "Silver"];
-  listOfTransmission = ["Manual", "Automatic"];
+  listOfBrands = ["ADANA", "ADIYAMAN", "AFYONKARAHİSAR", "AĞRI", "AMASYA", "ANKARA", "ANTALYA", "ARTVİN", "AYDIN", "BALIKESİR", "BİLECİK", "BİNGÖL", "BİTLİS", "BOLU", "BURDUR", "BURSA", "ÇANAKKALE", "ÇANKIRI", "ÇORUM", "DENİZLİ", "DİYARBAKIR", "EDİRNE", "ELAZIĞ", "ERZİNCAN", "ERZURUM", "ESKİŞEHİR", "GAZİANTEP", "GİRESUN", "GÜMÜŞHANE", "HAKKARİ", "HATAY", "IĞDIR", "ISPARTA", "İSTANBUL", "İZMİR", "KAHRAMANMARAŞ", "KARABÜK", "KARAMAN", "KARS", "KASTAMONU", "KAYSERİ", "KIRIKKALE", "KIRKLARELİ", "KIRŞEHİR", "KİLİS", "KOCAELİ", "KONYA", "KÜTAHYA", "MALATYA", "MANİSA", "MARDİN", "MERSİN", "MUĞLA", "MUŞ", "NEVŞEHİR", "NİĞDE", "ORDU", "OSMANİYE", "RİZE", "SAKARYA", "SAMSUN", "SİİRT", "SİNOP", "SİVAS", "ŞANLIURFA", "ŞIRNAK", "TEKİRDAĞ", "TOKAT", "TRABZON", "TUNCELİ", "UŞAK", "VAN", "YALOVA", "YOZGAT", "ZONGULDAK"];
+  listOfType = ["1+0", "1+1", "2+1", "3+1", "4+1", "5+1"];
+  listOfColor = ["1. Kat", "2. Kat", "3. Kat", "4. Kat", "5. Kat", "6. Kat", "7. Kat"];
+  listOfTransmission = ["0-50 m2", "50-75 m2", "75-100 m2", "100-150 m2", "150-200 m2"];
 
 
   constructor(private fb: FormBuilder, private service: AdminService) {
