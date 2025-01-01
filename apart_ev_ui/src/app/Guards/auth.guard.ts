e461ba06b084015c3e593fb0f8f76569aecfdecb
Furkan Kümüş <105515121+Furkan-Kumus@users.noreply.github.com>
2025-01-01 22:08:03 +0300

    guard added

---
+++ apart_ev_ui/src/app/Guards/auth.guard.ts
@@ -0,0 +1,7 @@
+import { CanActivateFn, Router, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
+import { Inject } from '@angular/core';
+import { session } from '../utils/session'; 
+
+export const authGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+  return false;
+};
--- apart_ev_ui/src/app/app-routing.module.ts
+++ apart_ev_ui/src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from './auth/components/signup/signup.component';
 import { LoginComponent } from './auth/components/login/login.component';
+import { authGuard } from './Guards/auth.guard';
 
 const routes: Routes = [
-    {path:"register", component: SignupComponent},
+    {path:"register", component: SignupComponent, canActivate: [authGuard]},
     {path:"login", component: LoginComponent},
     {path: "admin", loadChildren: () => import("./modules/admin/admin.module").then(m => m.AdminModule)},
     {path: "customer", loadChildren: () => import("./modules/customer/customer.module").then(m => m.CustomerModule)},
